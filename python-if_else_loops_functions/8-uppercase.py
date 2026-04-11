9672be09667f9aa944e48357ffe2e4aa5c0393ed
AyselBayramova <13868@holbertonstudents.com>
2026-04-11 16:48:02 +0000

    Fix uppercase function attribute error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-def print_last_digit(number):
-    """Prints the last digit of a number."""
-    last_digit = abs(number) % 10
-    print("{}".format(last_digit), end="")
-    return last_digit
+def uppercase(str):
+    """Prints a string in uppercase followed by a new line."""
+    for char in str:
+        char_code = ord(char)
+        if char_code >= 97 and char_code <= 122:
+            char_code = char_code - 32
+        print("{}".format(chr(char_code)), end="")
+    print("")

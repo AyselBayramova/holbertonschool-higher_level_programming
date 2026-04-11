ee146bd78043dcb69eb2ec07a798d94d2b5c16d5
AyselBayramova <13868@holbertonstudents.com>
2026-04-11 17:17:00 +0000

    Update task 100 with more readable logic

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-
-def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end="")
-        elif i % 3 == 0:
-            print("Fizz", end="")
-        elif i % 5 == 0:
-            print("Buzz", end="")
-        else:
-            print("{}".format(i), end="")
-
-        if i <= 100:
-            print(" ", end="")
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        char_code = i
+    else:
+        char_code = i - 32
+    print("{}".format(chr(char_code)), end="")

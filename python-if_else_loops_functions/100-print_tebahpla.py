c49f9a08e789dfd6454525f09c544ff72556d71a
AyselBayramova <13868@holbertonstudents.com>
2026-04-11 17:12:36 +0000

    Fix E501 line too long in 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
-# Loop from ASCII 'z' (122) down to 'a' (97)
-for char_code in range(122, 96, -1):
-    # Use conditional expression within format:
-    # If char_code is even, print it as is (lowercase).
-    # If char_code is odd, subtract 32 to get uppercase ASCII.
-    print("{}".format(chr(char_code if char_code % 2 == 0 else char_code - 32)), end="")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end="")
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif i % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print("{}".format(i), end="")
+
+        if i <= 100:
+            print(" ", end="")

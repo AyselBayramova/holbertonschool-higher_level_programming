dc8a44c436e99e927f66e43c5ec6340df5cf164d
AyselBayramova <13868@holbertonstudent.com>
2026-04-25 14:14:47 +0000

    Task complete

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,21 +7,12 @@ Module for text_indentation function.
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
-
-    Args:
-        text (str): The text to print.
-
-    Raises:
-        TypeError: If text is not a string.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # Xüsusi simvolları təyin edirik
     chars = [".", "?", ":"]
-    
     i = 0
-    # Mətnin əvvəlindəki boşluqları təmizləyirik
     text = text.strip()
 
     while i < len(text):
@@ -29,7 +20,6 @@ def text_indentation(text):
         if text[i] in chars:
             print("\n")
             i += 1
-            # Simvoldan sonra gələn bütün boşluqları keçirik (skip)
             while i < len(text) and text[i] == " ":
                 i += 1
             continue

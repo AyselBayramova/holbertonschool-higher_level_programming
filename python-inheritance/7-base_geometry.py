ba4aceefde3308a25678493c0c74b108af6d7db2
AyselBayramova <13868@holbertonstudents.com>
2026-04-25 20:56:18 +0000

    Final professional fix for all integer_validator cases

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""Module that defines a class BaseGeometry."""
+"""
+Module 5-base_geometry
+Defines a class BaseGeometry
+"""
 
 
 class BaseGeometry:
-    """A class representing base geometry."""
+    """Class BaseGeometry"""
 
     def area(self):
-        """Public instance method that raises an Exception."""
+        """Method that raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value as an integer."""
+        """
+        Validates value as an integer greater than 0.
+
+        Args:
+            name (str): The name of the value (always a string).
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:

9e6eb54f09743424b7042a306a426dbc8f50e35b
AyselBayramova <13868@holbertonstudents.com>
2026-04-25 18:08:55 +0000

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module for Square."""
+
+
+class Square:
+    """A class to define a square."
+
+    def__init__(self, size):
+       """Initialize a square.
+
+       Args:
+           size(init): Side length.
+       """
+       self.__size = size

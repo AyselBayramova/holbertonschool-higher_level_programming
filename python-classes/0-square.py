725ff53a49510dbb1b3b6a4f8b4511616cbf31e3
AyselBayramova <13868@holbertonstudents.com>
2026-04-25 17:45:23 +0000

    Add empty Square class

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines an empty Square class.
+
+The module provides a basic starting point for understanding
+Object-Oriented Programming in Python.
+"""
+
+
+class Square:
+    """An empty class that defines a square.
+
+    This class does not have any attributes or methods yet.
+    """
+    pass

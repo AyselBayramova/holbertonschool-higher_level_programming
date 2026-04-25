a632da7179ccb3fa9ce3a395329f45c03ec3ac6d
AyselBayramova <13868@holbertonstudents.com>
2026-04-25 18:58:37 +0000

    Task 4 complete

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,36 +5,37 @@ class Square:
     """This class represents a square."""
 
     def __init__(self, size=0):
-        """This class the size of the suqare.
+        """This sets the size of the square.
 
         Args:
             size (int): The size of the square.
         """
+        self.size = size
 
-        @proprety
-        def size(self):
-            """This metod gets the size.
+    @property
+    def size(self):
+        """This method gets the size.
 
-            Returns:
-                int: The size of the square.
-            """
+        Returns:
+            int: The size of the square.
+        """
 
-            return self.__size
-        
-        @size.setter
-        def size(self, value):
-            """This method sets the size with checks.
+        return self.__size
 
-            Args:
-                value (int): The new size.
-            """
+    @size.setter
+    def size(self, value):
+        """This method sets the size with checks.
 
-            if not isinstance(vale, int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+        Args:
+            value (int): The new size.
+        """
 
-        def area(self):
-            """This  method returns the area of the square."""
-            return self.__size * self.__size
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This method returns the area of the square."""
+        return self.__size * self.__size

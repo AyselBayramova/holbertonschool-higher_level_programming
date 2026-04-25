2a0b92a72b329b9bbbe85357a26a6229c56569b5
AyselBayramova <13868@holbertonstudent.com>
2026-04-25 13:58:03 +0000

    Correcting whitespace and indentation for checker

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,13 +7,6 @@ This module provides a function that prints a name.
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first name> <last name>".
-
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print. Defaults to an empty string.
-
-    Raises:
-        TypeError: If either first_name or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")

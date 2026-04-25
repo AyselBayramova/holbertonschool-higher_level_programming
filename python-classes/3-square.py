e1131ba8d2b1286db0dc372c805e9b70c545d702
AyselBayramova <13868@holbertonstudents.com>
2026-04-25 18:44:02 +0000

    Fix documentation and formatting

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""This module is for a Square class with area."""
+"""Module that defines a Square class with area calculation."""
+
 
 class Square:
-    """This class is a square."""
+    """This class represents a square."""
+
     def __init__(self, size=0):
-        """This sets the size of the square.
+        """Initializes the square with size validation.
 
         Args:
-            size (int): The side of the square.
+            size (int): The size of the square.
         """
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
-    def area(self):
-        """This returns the area of the square."""
 
+    def area(self):
+        """Method that returns the current area of the square."""
         return self.__size * self.__size

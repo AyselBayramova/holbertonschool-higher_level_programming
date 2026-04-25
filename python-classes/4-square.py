b4f387f686b4018a19199d6e8bacca707fb5b6a3
AyselBayramova <13868@holbertonstudents.com>
2026-04-25 18:55:21 +0000

    Task 4 complete

---
+++ python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with getter and setter."""
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size=0):
+        """This class the size of the suqare.
+
+        Args:
+            size (int): The size of the square.
+        """
+
+        @proprety
+        def size(self):
+            """This metod gets the size.
+
+            Returns:
+                int: The size of the square.
+            """
+
+            return self.__size
+        
+        @size.setter
+        def size(self, value):
+            """This method sets the size with checks.
+
+            Args:
+                value (int): The new size.
+            """
+
+            if not isinstance(vale, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+        def area(self):
+            """This  method returns the area of the square."""
+            return self.__size * self.__size
